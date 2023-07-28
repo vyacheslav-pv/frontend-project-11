73f9d759a52896b284bbe9f55d714a4034cc46ea
Vyacheslav Pilyugin <vyach-pvv@yandex.ru>
2023-07-28 09:39:56 +0700

    Add locales (ru)

---
+++ src/locales/ru.js
@@ -0,0 +1,16 @@
+export default {
+  translation: {
+    posts: 'Посты',
+    feeds: 'Фиды',
+    view: 'Просмотр',
+    formFeedback: {
+      success: 'RSS успешно загружен',
+      errors: {
+        invalidUrl: 'Ссылка должна быть валидным URL',
+        parserError: 'Ресурс не содержит валидный RSS',
+        duplicateUrl: 'RSS уже существует',
+        emptyField: 'Не должно быть пустым',
+      },
+    },
+  },
+};
