2c669ff74721b7d4173c82bfc6802ba4a789ff12
Vyacheslav Pilyugin <vyach-pvv@yandex.ru>
2023-08-20 13:42:53 +0700

    refactor: initialize i18next

--- src/init.js
+++ src/init.js
@@ -36,9 +36,10 @@ const updateRSS = (watchedState, urlProx, feedId) => {
         return false;
       }
 
-      newPosts.forEach((post) => {
-        post.feedId = feedId; // eslint-disable-line no-param-reassign
-        post.id = uniqueId(); // eslint-disable-line no-param-reassign
+      newPosts.forEach((newpost) => {
+        const post = newpost;
+        post.feedId = feedId;
+        post.id = uniqueId();
       });
       watchedState.data.posts.push(...newPosts);
       return newPosts;
@@ -47,13 +48,6 @@ const updateRSS = (watchedState, urlProx, feedId) => {
 };
 
 export default () => {
-  const i18nextInstance = i18n.createInstance();
-  i18nextInstance.init({
-    lng: 'ru',
-    debug: false,
-    resources,
-  });
-
   yup.setLocale({
     mixed: {
       notOneOf: 'formFeedback.errors.duplicateUrl',
@@ -64,117 +58,125 @@ export default () => {
     },
   });
 
-  const elements = {
-    form: document.querySelector('.rss-form'),
-    posts: document.querySelector('.posts'),
-    feeds: document.querySelector('.feeds'),
-    input: document.getElementById('url-input'),
-    submitButton: document.querySelector('button[type="submit"]'),
-    feedback: document.querySelector('.feedback'),
-    modalTitle: document.querySelector('.modal-title'),
-    modalDescription: document.querySelector('.modal-body'),
-    modalFullArticle: document.querySelector('.full-article'),
-  };
+  const i18nextInstance = i18n.createInstance();
+  i18nextInstance.init({
+    lng: 'ru',
+    debug: false,
+    resources,
+  })
+    .then(() => {
+      const elements = {
+        form: document.querySelector('.rss-form'),
+        posts: document.querySelector('.posts'),
+        feeds: document.querySelector('.feeds'),
+        input: document.getElementById('url-input'),
+        submitButton: document.querySelector('button[type="submit"]'),
+        feedback: document.querySelector('.feedback'),
+        modalTitle: document.querySelector('.modal-title'),
+        modalDescription: document.querySelector('.modal-body'),
+        modalFullArticle: document.querySelector('.full-article'),
+      };
 
-  const initialState = {
-    processState: 'initialized',
-    form: {
-      feedback: null,
-      processState: 'filling',
-    },
-    validatedLinks: [],
-    uiState: {
-      readPostsId: new Set(),
-      modalPostId: null,
-    },
-    data: {
-      feeds: [],
-      posts: [],
-    },
-  };
+      const initialState = {
+        processState: 'initialized',
+        form: {
+          feedback: null,
+          processState: 'filling',
+        },
+        validatedLinks: [],
+        uiState: {
+          readPostsId: new Set(),
+          modalPostId: null,
+        },
+        data: {
+          feeds: [],
+          posts: [],
+        },
+      };
 
-  const watchedState = state(initialState, elements, i18nextInstance);
+      const watchedState = state(initialState, elements, i18nextInstance);
 
-  elements.form.addEventListener('submit', (ev) => {
-    ev.preventDefault();
-    watchedState.form.processState = 'validating';
-    const formData = new FormData(ev.target);
-    const url = formData.get('url');
+      elements.form.addEventListener('submit', (ev) => {
+        ev.preventDefault();
+        watchedState.form.processState = 'validating';
+        const formData = new FormData(ev.target);
+        const url = formData.get('url');
 
-    let urlProx;
+        let urlProx;
 
-    const schema = urlSchema(watchedState.validatedLinks);
+        const schema = urlSchema(watchedState.validatedLinks);
 
-    schema.validate(url)
-      .then(() => {
-        watchedState.form.processState = 'validated';
-        watchedState.processState = 'loading';
-        watchedState.validatedLinks.push(url);
-        urlProx = getUrlProxy(url);
-        return axios.get(urlProx);
-      })
-      .then((response) => response.data.contents)
-      .then((content) => {
-        const parsedContent = parse(content);
-        const { currentFeed, currentPosts } = parsedContent;
+        schema.validate(url)
+          .then(() => {
+            watchedState.form.processState = 'validated';
+            watchedState.processState = 'loading';
+            watchedState.validatedLinks.push(url);
+            urlProx = getUrlProxy(url);
+            return axios.get(urlProx);
+          })
+          .then((response) => response.data.contents)
+          .then((content) => {
+            const parsedContent = parse(content);
+            const { currentFeed, currentPosts } = parsedContent;
 
-        if (!currentFeed || !currentPosts) {
-          throw new Error('Parser Error');
-        }
-
-        currentFeed.id = uniqueId();
-        currentPosts.forEach((post) => {
-          // eslint-disable-next-line no-param-reassign
-          post.feedId = currentFeed.id;
-          // eslint-disable-next-line no-param-reassign
-          post.id = uniqueId();
-        });
-
-        watchedState.data.feeds.push(currentFeed);
-        watchedState.data.posts.push(...currentPosts);
-
-        watchedState.processState = 'added';
-        watchedState.form.processState = 'filling';
-        watchedState.form.feedback = 'formFeedback.success';
-        return currentFeed.id;
-      })
-      .then((feedId) => {
-        watchedState.processState = 'monitoring';
-        return setTimeout(() => updateRSS(watchedState, urlProx, feedId), 5000);
-      })
-      .catch((e) => {
-        switch (e.name) {
-          case 'ValidationError': {
-            const [errorCode] = e.errors;
-            watchedState.form.feedback = errorCode;
-            watchedState.form.processState = 'invalidated';
-            break;
-          }
-
-          case 'AxiosError':
-            if (e.message === 'Network Error') {
-              watchedState.processState = 'networkError';
-              watchedState.form.feedback = 'formFeedback.errors.network';
+            if (!currentFeed || !currentPosts) {
+              throw new Error('Parser Error');
             }
-            break;
 
-          case 'Error':
-            if (e.message === 'Parser Error') {
-              watchedState.processState = 'parserError';
-              watchedState.form.feedback = 'formFeedback.errors.parserError';
+            currentFeed.id = uniqueId();
+            currentPosts.forEach((post) => {
+            // eslint-disable-next-line no-param-reassign
+              post.feedId = currentFeed.id;
+              // eslint-disable-next-line no-param-reassign
+              post.id = uniqueId();
+            });
+
+            watchedState.data.feeds.push(currentFeed);
+            watchedState.data.posts.push(...currentPosts);
+
+            watchedState.processState = 'added';
+            watchedState.form.processState = 'filling';
+            watchedState.form.feedback = 'formFeedback.success';
+            return currentFeed.id;
+          })
+          .then((feedId) => {
+            watchedState.processState = 'monitoring';
+            return setTimeout(() => updateRSS(watchedState, urlProx, feedId), 5000);
+          })
+          .catch((e) => {
+            switch (e.name) {
+              case 'ValidationError': {
+                const [errorCode] = e.errors;
+                watchedState.form.feedback = errorCode;
+                watchedState.form.processState = 'invalidated';
+                break;
+              }
+
+              case 'AxiosError':
+                if (e.message === 'Network Error') {
+                  watchedState.processState = 'networkError';
+                  watchedState.form.feedback = 'formFeedback.errors.network';
+                }
+                break;
+
+              case 'Error':
+                if (e.message === 'Parser Error') {
+                  watchedState.processState = 'parserError';
+                  watchedState.form.feedback = 'formFeedback.errors.parserError';
+                }
+                break;
+
+              default:
+                throw new Error(`Unknown error ${e}`);
             }
-            break;
-
-          default:
-            throw new Error(`Unknown error ${e}`);
+          });
+      });
+      elements.posts.addEventListener('click', (e) => {
+        if (e.target.dataset.id) {
+          const readPostsId = e.target.dataset.id;
+          watchedState.uiState.modalPostId = readPostsId;
+          watchedState.uiState.readPostsId.add(readPostsId);
         }
       });
-  });
-  elements.posts.addEventListener('click', (e) => {
-    if (e.target.dataset.id) {
-      const readPostsId = e.target.dataset.id;
-      watchedState.uiState.modalPostId = readPostsId;
-      watchedState.uiState.readPostsId.add(readPostsId);
-    }
-  });
+    });
 };
